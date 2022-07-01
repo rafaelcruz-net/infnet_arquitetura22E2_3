f8478c138ef4046bb6b33ae88857dd9e351c8359
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2022-06-30 21:09:15 -0300

    Habilitando o swagger

--- spotify-lite/spotifylite/spotifylite/Program.cs
+++ spotify-lite/spotifylite/spotifylite/Program.cs
@@ -38,12 +38,8 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
